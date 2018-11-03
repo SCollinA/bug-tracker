c14f24ce6ba274a5130d1e1249db0ced091d31e6
Collin Argo <collinargo@gmail.com>
2018-11-03 13:01:51 -0400

    style expandedBug div

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const bugList = document.getElementsByClassName('bugList')[0]
 
+
+// adds a bunch of dummy bugs
 for (let i = 0; i < 20; i++) {
     const bug = document.getElementsByClassName('bug')[0].cloneNode(true)
     bugList.appendChild(bug)
