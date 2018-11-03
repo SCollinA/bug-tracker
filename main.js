ca9909486810aff1a43f76b8d6ca0de20755304f
Collin Argo <collinargo@gmail.com>
2018-11-03 13:10:10 -0400

    add dummy bugUpdates to bugs, style

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const bugList = document.getElementsByClassName('bugList')[0]
 
-
 // adds a bunch of dummy bugs
 for (let i = 0; i < 20; i++) {
     const bug = document.getElementsByClassName('bug')[0].cloneNode(true)
     bugList.appendChild(bug)
 }
+
+const expandedBugs = document.querySelectorAll('.expandedBug')
+
+expandedBugs.forEach(expandedBug => {
+    for (let i = 0; i < 5; i++) {
+        const bugUpdate = document.getElementsByClassName('bugUpdate')[0].cloneNode(true)
+        expandedBug.appendChild(bugUpdate)
+    }
+})
